1a2f5ecc6b42f903a000bf8ca1020e1ab25f1d32
AlexKysil <38106741+AlexKysil@users.noreply.github.com>
2021-11-20 15:03:04 +0200

    start fields

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+
+from chromedriver_py import binary_path as chrome_path
+
+
+@pytest.fixture(scope='module')
+def driver_session():
+    """
+    Init chromedriver session
+    :return:
+    """
+
+    driver_session = webdriver.Chrome(executable_path=chrome_path)
+    driver_session.maximize_window()
+    driver_session.implicitly_wait(5)
+
+    yield driver_session
+
+    driver_session.quit()
+
+
+@pytest.fixture(scope='function')
+def data_storage():
+    """
+    Hold data during test
+    :return:
+    """
+
+    data_storage = {}
+    yield data_storage
---
+++ tests/test_youtube.py
@@ -0,0 +1,6 @@
+
+def test_youtube_search_field(driver_session):
+    pass
+
+def test_youtube_home_btn(driver_session):
+    pass
